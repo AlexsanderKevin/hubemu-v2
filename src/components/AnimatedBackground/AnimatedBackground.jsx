b7b89b5d67758474c991d963cc6ded2fae7b3010
KEVIN ALEXSANDER DE JESUS <1050482123033@fatec.coorp>
2024-05-04 10:36:24 -0300

    fix files

--- src/components/AnimatedBackground/AnimatedBackground.jsx
+++ src/components/AnimatedBackground/AnimatedBackground.jsx
@@ -1,25 +1,12 @@
 import styles from './AnimatedBackground.module.css'
-// import saturnSvg from '../../static/svg/svg-saturn.svg'
 import { ReactComponent as SaturnSvg } from '../../static/svg/svg-saturn.svg'
-// import ilustrationGalaxy from '../../static/svg/ilustration-galaxy.svg'
 import { ReactComponent as IlustrationGalaxy } from '../../static/svg/ilustration-galaxy.svg'
-// import { ReactComponent as MySVG } from './my-svg.svg';
 
 export default function AnimatedBackground() {
   return (
     <div className={styles.bg}>
       <SaturnSvg className={`${styles.saturnSvg}`}/>
-      {/* <img 
-        src={saturnSvg}
-        className={`${styles.saturnSvg}`}
-        alt='saturn illustration'
-      /> */}
       <IlustrationGalaxy className={`${styles.ilustrationGalaxy}`}/>
-      {/* <img 
-        src={ilustrationGalaxy}
-        className={`${styles.ilustrationGalaxy}`}
-        alt='galaxy illustration'
-      /> */}
       <div className={styles.square}></div>
       <div className={styles.square}></div>
       <div className={styles.square}></div>
