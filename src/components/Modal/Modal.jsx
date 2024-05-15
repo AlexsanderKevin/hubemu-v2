4abd1a64a3b5256ec9df2ecb7d4bd82a41ceb245
ChesterKing66 <kevin13348@gmail.com>
2024-05-15 20:19:15 -0300

    started the modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,15 +10,22 @@ export default function Modal() {
 
     <div className={styles.modal}>
       <div className={styles.modalContainer}>
-        <div className={styles.modalHeader}>
-          header
-        </div>
-        <div className={styles.modalBody}>
-          body
-        </div>
-        <div className={styles.modalFooter}>
-          footer
-        </div>
+        <form action="">
+          <div className={styles.modalHeader}>
+            Adicionar plataforma
+          </div>
+          <div className={styles.modalBody}>
+            <p>Você ainda não configurou nenhuma plataforma. Insira o caminho para o diretório em que seu emulador está instalado</p>
+            <div className={styles.inputContainer}>
+              <label htmlFor="directory-input">Caminho do  emulador</label>
+              <input type="text" name='directory-input' id='directory-input' required/>
+            </div>
+          </div>
+          <div className={styles.modalFooter}>
+            <button type='submit'>Voltar</button>
+            <button type='submit'>Próximo</button>
+          </div>
+        </form>
       </div>
     </div>
 
